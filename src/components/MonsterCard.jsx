ef50a68a3ff24ee055c0baf3ae0f78cb81d3d635
Julien B <gercevalcode@gmail.com>
2019-10-31 00:00:49 +0100

    fight method working

--- src/components/MonsterCard.jsx
+++ src/components/MonsterCard.jsx
@@ -7,6 +7,7 @@ function randomMonster(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+
 class MonsterCard extends Component {
   constructor(props) {
     super(props);
@@ -16,30 +17,43 @@ class MonsterCard extends Component {
     };
     this.getMonster1 = this.getMonster1.bind(this);
     this.getMonster2 = this.getMonster2.bind(this);
+    this.fight = this.fight.bind(this);
   }
+  
   componentDidMount() {
     this.getMonster1();
     this.getMonster2();
+    this.fight();
   }
+  
+  componentWillUpdate() {
+  }
+  
   getMonster1() {
     axios
-      .get('https://hackathon-wild-hackoween.herokuapp.com/monsters')
-      .then(response => response.data)
-      .then(data => data.monsters[randomMonster(20)])
-      .then(MonsterInfo => {
-        this.setState({ Monster1: MonsterInfo });
-      });
+    .get('https://hackathon-wild-hackoween.herokuapp.com/monsters')
+    .then(response => response.data)
+    .then(data => data.monsters[randomMonster(20)])
+    .then(MonsterInfo => {
+      this.setState({ Monster1: MonsterInfo });
+    });
   }
-
+  
   getMonster2() {
     axios
-      .get('https://hackathon-wild-hackoween.herokuapp.com/monsters')
-      .then(response => response.data)
-      .then(data => data.monsters[randomMonster(20)])
-      .then(MonsterInfo => {
-        this.setState({ Monster2: MonsterInfo });
-      });
+    .get('https://hackathon-wild-hackoween.herokuapp.com/monsters')
+    .then(response => response.data)
+    .then(data => data.monsters[randomMonster(20)])
+    .then(MonsterInfo => {
+      this.setState({ Monster2: MonsterInfo });
+    });
   }
+  
+  
+  fight() {
+      this.state.Monster2 && this.setState({ Monster2: {...this.state.Monster2, defense: (this.state.Monster2.defense - this.state.Monster1.attack)} })
+  }
+  
 
   render() {
     return (
@@ -56,8 +70,13 @@ class MonsterCard extends Component {
           </button>
           {this.state.Monster2 && <MonsterInfo infos={this.state.Monster2} />}
         </div>
+        <div className="fightButton">
+          <button onClick={this.fight}></button>
+        </div>
       </div>
     );
   }
 }
+
+
 export default MonsterCard;
