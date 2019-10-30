2c0c03a9a932eb6f80063271d63f92cd10069c17
Julien B <gercevalcode@gmail.com>
2019-10-30 15:33:28 +0100

    created DisplayCard.jsx with a  class component and infos en dur

---
+++ src/components/DisplayCard.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+const monster = {
+  id: 18,
+  name: 'The Reaper',
+  level: '5',
+  attack: 12,
+  defense: 8,
+  special: 'Flying',
+  description: 'In the Maelstrom, a trick of the light can feast on human flesh',
+  picture: 'https://nsa40.casimages.com/img/2019/10/23/191023045327211233.jpg'
+}
+
+
+class DisplayCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default DisplayCard;
+
+
